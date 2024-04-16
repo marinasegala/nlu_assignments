a3206a5a9990c5830368ab9ff458cf242c45e425
marinasegala <marinasegala01@gmail.com>
2024-04-16 17:05:57 +0200

    try1 no dropout, sgd and lstm

--- LM/part_1/model.py
+++ LM/part_1/model.py
@@ -7,21 +7,21 @@ class LM_LSTM(nn.Module):
         # Token ids to vectors, we will better see this in the next lab
         self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
 
-        self.emb_dropout = nn.Dropout(emb_dropout)
+        #self.emb_dropout = nn.Dropout(emb_dropout)
 
         self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)
         self.pad_token = pad_index
 
-        self.last_dropout = nn.Dropout(out_dropout)
+        #self.last_dropout = nn.Dropout(out_dropout)
 
         # Linear layer to project the hidden layer to our output space
         self.output = nn.Linear(hidden_size, output_size)
 
     def forward(self, input_sequence):  #how layers interact
         emb = self.embedding(input_sequence)
-        drop1 = self.emb_dropout(emb)
-        lsmt_out, _  = self.lstm(drop1) #emb
-        drop2 = self.last_dropout(lsmt_out)
-        output = self.output(drop2).permute(0,2,1) #rnn_out
+        #drop1 = self.emb_dropout(emb)
+        lsmt_out, _  = self.lstm(emb) #emb
+        #drop2 = self.last_dropout(lsmt_out)
+        output = self.output(lsmt_out).permute(0,2,1) #rnn_out
         return output
     
